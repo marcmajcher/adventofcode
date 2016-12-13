f50be4c46b4af3728c3d3c9a7fd3ad4d29df08b3
Marc Majcher <majcher@gmail.com>
2016-12-13 16:27:55 -0600

    day 4.1

--- day04/04_security.js
+++ day04/04_security.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const sampleData = [
-  'aaaaa-bbb-z-y-x-123[abxyz]', // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
-  'a-b-c-d-e-f-g-h-987[abcde]', // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
-  'not-a-real-room-404[oarel]', // is a real room.
-  'totally-real-room-200[decoy]' // is not.
-]; // => 1514
+// const sampleData = [
+//   'aaaaa-bbb-z-y-x-123[abxyz]', // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
+//   'a-b-c-d-e-f-g-h-987[abcde]', // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
+//   'not-a-real-room-404[oarel]', // is a real room.
+//   'totally-real-room-200[decoy]' // is not.
+// ]; // => 1514
 
-// const sampleData = require('./data');
+const sampleData = require('./data');
 let total = 0;
 
 sampleData.forEach(addIfReal);
@@ -20,15 +20,15 @@ function addIfReal(data) {
   }, {});
   let sum = Object.keys(letters).sort()
     .sort((a, b) => {
+      if (letters[a] === letters[b]) {
+        return (a<b?-1:a>b);
+      }
       return letters[b] - letters[a]
     })
     .join('');
 
   if (sum.slice(0, 5) === check) {
     total += parseInt(id);
-  }
-  else {
-    console.log(name, letters, sum, sum.slice(0, 5), check);
   }
 }
 
