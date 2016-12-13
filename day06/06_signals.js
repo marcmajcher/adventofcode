e97aeb575d1fb5788b7ed5c45eea2ef68f0adc85
Marc Majcher <majcher@gmail.com>
2016-12-13 17:26:21 -0600

    day6

---
+++ day06/06_signals.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// const data = require('./data');
+
+const data = `
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+`; // => easter
