58914a5a3233c301ce16c82fb52ae8e596555ca8
Marc Majcher <majcher@gmail.com>
2016-12-13 16:09:08 -0600

    day4.1 wip

--- day04/04_security.js
+++ day04/04_security.js
@@ -1,8 +1,35 @@
 'use strict';
 
 const sampleData = [
-'aaaaa-bbb-z-y-x-123[abxyz]',  // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
-'a-b-c-d-e-f-g-h-987[abcde]',  // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
-'not-a-real-room-404[oarel]',  // is a real room.
-'totally-real-room-200[decoy]' // is not.
-];
+  'aaaaa-bbb-z-y-x-123[abxyz]', // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
+  'a-b-c-d-e-f-g-h-987[abcde]', // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
+  'not-a-real-room-404[oarel]', // is a real room.
+  'totally-real-room-200[decoy]' // is not.
+]; // => 1514
+
+// const sampleData = require('./data');
+let total = 0;
+
+sampleData.forEach(addIfReal);
+
+function addIfReal(data) {
+  let [_, name, id, check] = data.match(/^([a-z\-]+)-(\d+)\[(\w+)]$/);
+  let letters = name.replace(/\W/g, '').split('').reduce((obj, cur) => {
+    obj[cur] = obj[cur] ? obj[cur] + 1 : 1;
+    return obj;
+  }, {});
+  let sum = Object.keys(letters).sort()
+    .sort((a, b) => {
+      return letters[b] - letters[a]
+    })
+    .join('');
+
+  if (sum.slice(0, 5) === check) {
+    total += parseInt(id);
+  }
+  else {
+    console.log(name, letters, sum, sum.slice(0, 5), check);
+  }
+}
+
+console.log(total);
