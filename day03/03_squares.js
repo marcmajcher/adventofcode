58054fe77ccc2eb71baa32502f2e9525383ad0ca
Marc Majcher <majcher@gmail.com>
2016-12-13 12:19:49 -0600

    day3.1

--- day03/03_squares.js
+++ day03/03_squares.js
@@ -1,3 +1,9 @@
 const sampleData = require('./data');
 
-console.log(sampleData);
+
+console.log(sampleData.reduce(isValid, 0));
+
+function isValid(last, cur) {
+  triangle = cur.sort((a, b) => { return a > b });
+  return (triangle[0] + triangle[1] > triangle[2] ? last + 1 : last);
+}
