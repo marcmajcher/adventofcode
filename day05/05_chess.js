d5ff7746c497ac7b5e0d921ed2bfb111e2cff9ce
Marc Majcher <majcher@gmail.com>
2016-12-13 17:22:54 -0600

    day5.2

--- day05/05_chess.js
+++ day05/05_chess.js
@@ -4,16 +4,21 @@ const md5 = require('md5');
 const input = 'reyedfim';
 let index = 0;
 let found = 0;
-let code = '';
+let code = '--------'.split('');
 
 while (found < 8) {
-  let hash = md5(input+index);
+  let hash = md5(input + index);
   if (hash.match(/^00000/)) {
-    let m = hash.match(/^00000(.)/);
-    code += m[1];
-    found++;
+    let m = hash.match(/^00000(.)(.)/);
+    if (m[1] >= 0 && m[1] <= 7) {
+      if (code[m[1]] === '-') {
+        code[m[1]] = m[2];
+        console.log(code.join(''));
+        found++;
+      }
+    }
   }
   index++;
 }
 
-console.log(code);
+console.log(code.join(''));
