db2ce7248fa3295408d48d8598e458c33367eb87
Marc Majcher <majcher@gmail.com>
2016-12-13 21:32:21 -0600

    day7.2

--- day07/ipv7.js
+++ day07/ipv7.js
@@ -3,20 +3,29 @@
 const data = require('./data');
 
 // const data = [
-//   'abba[mnop]qrst', // supports TLS (abba outside square brackets).
-//   'abcd[bddb]xyyx', // does not support TLS (bddb is within square brackets, even though xyyx is outside square brackets).
-//   'aaaa[qwer]tyuibcf', // does not support TLS (aaaa is invalid; the interior characters must be different).
-//   'ioxxoj[asdfgh]zxcvbn' // supports TLS (oxxo is outside square brackets, even though it's within a larger string).
+//   'aba[ssbabz]xyz', // supports SSL (aba outside square brackets with corresponding bab within square brackets).
+//   'xyx[xyx]xyx', // does not support SSL (xyx, but no corresponding yxy).
+//   'aaa[kek]eke', // supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
+//   'zazbz[zbzb]cdb' // supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
 // ];
 
 let count = 0;
 
 data.forEach((ip) => {
-  if (!ip.match(/\[\w*(.)(.)\2\1\w*\]/)) {
-    let m = ip.match(/(.)(.)\2\1/g);
-    if (m !== null) {
-      for (let i = 0; i < m.length; i++) {
-        if (!m[i].match(/(.)\1{3}/)) {
+  let out = true;
+  for (let i = 0; i < ip.length - 2; i++) {
+    if (ip[i] === '[') {
+      out = false;
+    }
+    else if (ip[i] === ']') {
+      out = true;
+    }
+    if (out) {
+      let a = ip.slice(i, i + 3);
+      if (a[0] === a[2] && a[0] !== a[1] && a[1] !== '[' && a[1] !== ']') {
+        let b = a[1] + a[0] + a[1];
+        let re = new RegExp('\\[\\w*'+b+'\\w*\\]');
+        if (ip.match(re)) {
           console.log(ip);
           count++;
           break;
