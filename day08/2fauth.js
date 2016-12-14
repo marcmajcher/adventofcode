053ab09da0ce8cee14f05eaec299244915c64ed0
Marc Majcher <majcher@gmail.com>
2016-12-13 21:49:36 -0600

    day8

--- day08/2fauth.js
+++ day08/2fauth.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const rows = 6;
+const cols = 50;
+const grid = [];
+const off = '.';
+const on = '#';
+
+for (let i = 0; i < rows; i++) {
+  grid[i] = new Array(cols);
+  grid[i].fill(off);
+}
+
+function printGrid() {
+  for (let i = 0; i < rows; i++) {
+    console.log(grid[i].join(''));
+  }
+}
+
+function countGrid() {
+  let count = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === on) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+printGrid();
+console.log(countGrid());
