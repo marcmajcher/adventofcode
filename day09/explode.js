ae567764a4ce3654d293f5670cf94ce0cb9e4d2f
Marc Majcher <majcher@gmail.com>
2016-12-14 00:26:30 -0600

    day9.2 whew

--- day09/explode.js
+++ day09/explode.js
@@ -2,16 +2,60 @@
 
 let data = require('./data');
 
-// let data = 'ADVENT'; // contains no markers and decompresses to itself with no changes, resulting in a decompressed length of 6.
-// let data = 'A(1x5)BC'; // repeats only the B a total of 5 times, becoming ABBBBBC for a decompressed length of 7.
-// let data = '(3x3)XYZ'; // becomes XYZXYZXYZ for a decompressed length of 9.
-// let data = 'A(2x2)BCD(2x2)EFG'; // doubles the BC and EF, becoming ABCBCDEFEFG for a decompressed length of 11.
-// let data = '(6x1)(1x3)A'; // simply becomes (1x3)A - the (1x3) looks like a marker, but because it's within a data section of another marker, it is not treated any differently from the A that comes after it. It has a decompressed length of 6.
-// let data = 'X(8x2)(3x3)ABCY'; // becomes X(3x3)ABC(3x3)ABCY (for a decompressed length of 18), because the decompressed data from the (8x2) marker (the (3x3)ABC) is skipped and not processed further.
+// let data = '(3x3)XYZ'; // still becomes XYZXYZXYZ, as the decompressed section contains no markers.
+// let data = 'X(8x2)(3x3)ABCY'; //  becomes XABCABCABCABCABCABCY, because the decompressed data from the (8x2) marker is then further decompressed, thus triggering the (3x3) marker twice for a total of six ABC sequences.
+// let data = '(27x12)(20x12)(13x14)(7x10)(1x12)A'; //  decompresses into a string of A repeated 241920 times.
+// let data = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'; //  becomes 445 characters long.
 
-let d = decode(data);
-console.log(d)
-console.log(d.length);
+let dc = decodeCount(data);
+console.log(dc);
+// console.log(d)
+// console.log(d.length);
+
+function decodeCount(str) {
+  let weights = new Array(data.length);
+  weights.fill(1);
+  let total = 0;
+  let marker = false;
+  let charCount = '';
+  let times = '';
+  let x = false;
+  for (var i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char === '(') {
+      marker = true;
+      continue;
+    }
+    else if (char === ')') {
+      for (let n = i + 1; n <= i + parseInt(charCount); n++) {
+        weights[n] *= parseInt(times);
+      }
+
+      marker = false;
+      x = false;
+      charCount = '';
+      times = '';
+    }
+    else {
+      if (marker) {
+        if (char === 'x') {
+          x = true;
+          continue;
+        }
+        else if (x) {
+          times += char;
+        }
+        else {
+          charCount += char;
+        }
+      }
+      else {
+        total += weights[i];
+      }
+    }
+  }
+  return total;
+}
 
 function decode(str) {
   const re = /^(.*?)\(([x\d]+)\)(.*)$/;
@@ -19,15 +63,14 @@ function decode(str) {
   let m = str.match(re);
   while (m !== null) {
     let [_, pre, marker, rest] = str.match(re);
-    // console.log(pre, marker, rest);
-    decoded += pre;
+    // console.log('--> ',pre, marker, rest);
     let [len, rep] = marker.split('x');
+    let decoded = '';
     for (let i = 0; i < rep; i++) {
       decoded += rest.slice(0, len);
     }
-    str = rest.slice(len);
+    str = pre + decoded + rest.slice(len);
     m = str.match(re);
   }
-  decoded += str;
-  return decoded;
+  return str;
 }
