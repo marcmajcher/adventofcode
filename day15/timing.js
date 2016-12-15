75d5398afa95b06c89780e532c0201f76eb5502f
Marc Majcher <majcher@gmail.com>
2016-12-15 00:46:43 -0600

    day15.1

--- day15/timing.js
+++ day15/timing.js
@@ -1,8 +1,46 @@
-/*
-Disc #1 has 17 positions; at time=0, it is at position 15.
-Disc #2 has 3 positions; at time=0, it is at position 2.
-Disc #3 has 19 positions; at time=0, it is at position 4.
-Disc #4 has 13 positions; at time=0, it is at position 2.
-Disc #5 has 7 positions; at time=0, it is at position 2.
-Disc #6 has 5 positions; at time=0, it is at position 0.
-*/
+'use strict';
+
+const discs = [{
+  size: 17,
+  pos: 15
+}, {
+  size: 3,
+  pos: 2
+}, {
+  size: 19,
+  pos: 4
+}, {
+  size: 13,
+  pos: 2
+}, {
+  size: 7,
+  pos: 2
+}, {
+  size: 5,
+  pos: 0
+}];
+
+// const discs = [
+//   {size: 5, pos: 4},
+//   {size: 2, pos: 1}
+// ];
+
+let time = -1;
+let ok = false;
+
+while (!ok) {
+  time++;
+  ok = true;
+
+  console.log('Time', time);
+  for (let i = 0; i < discs.length; i++) {
+    const sec = i + 1;
+    const pos = (discs[i].pos + sec + time) % discs[i].size;
+    // console.log('  sec',sec,': disc',i,'at',pos);
+    if (pos !== 0) {
+      ok = false;
+      break;
+    }
+  }
+}
+console.log(time);
