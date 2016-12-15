05668a5bcc0b93b7dda32336a0b7bfad806cbbf0
Marc Majcher <majcher@gmail.com>
2016-12-15 01:35:33 -0600

    day14.2

--- day14/otp.js
+++ day14/otp.js
@@ -6,17 +6,32 @@ const salt = 'yjdafjpo';
 let index = -1;
 let hashes = [];
 
+let cache = {};
+
+function md5s(str) {
+  const orig = str;
+  if (cache[orig]) {
+    return cache[orig];
+  }
+  for (let i = 0; i < 2017; i++) {
+    str = md5(str);
+  }
+  cache[orig] = str;
+  return str;
+}
+
 while (hashes.length < 64) {
   index++;
-  const hash = md5(salt + index);
+  const hash = md5s(salt + index);
   const m = hash.match(/(.)\1\1/);
   if (m) {
     const char = m[1];
     const re = new RegExp(`(${char})\\1{4}`);
     for (var i = 1; i <= 1000; i++) {
-      const index2 = index+i;
-      const hash2 = md5(salt + index2);
+      const index2 = index + i;
+      const hash2 = md5s(salt + index2);
       if (hash2.match(re)) {
+        console.log(index, hash);
         hashes.push({
           index,
           hash
@@ -26,5 +41,5 @@ while (hashes.length < 64) {
     }
   }
 }
-console.log(hashes);
+// console.log(hashes);
 console.log(index);
