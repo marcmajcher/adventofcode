5b52382ee1780a7165db6d879a3f2a7360c7234c
Marc Majcher <majcher@gmail.com>
2016-12-15 13:15:01 -0600

    day13 different map

--- day13/maze.js
+++ day13/maze.js
@@ -5,7 +5,6 @@ const pq = require('js-priority-queue');
 // const data = 1352;
 const data = 10;
 
-const size = 10;
 const grid = [];
 const start = {
   x: 1,
@@ -17,26 +16,53 @@ const dest = {
 };
 
 function getPoint(x, y) {
-  let num = x * x + 3 * x + 2 * x * y + y + y * y + data;
-  let bin = num.toString(2);
-  let ones = bin.split('1').length - 1;
-  return (ones & 1) ? '#' : '.';
+  if (!grid[y]) {
+    grid[y] = [];
+  }
+  if (!grid[y][x]) {
+    let num = x * x + 3 * x + 2 * x * y + y + y * y + data;
+    let bin = num.toString(2);
+    let ones = bin.split('1').length - 1;
+    grid[y][x] = (ones & 1) ? '#' : '.';
+  }
+  return grid[y][x]
 }
 
-function fillGrid() {
-  for (let y = 0; y < size; y++) {
-    grid[y] = [];
-    for (let x = 0; x < size; x++) {
-      grid[y][x] = getPoint(x, y);
-    }
-  }
-}
+getPoint(0, 0);
+getPoint(0, 1);
+getPoint(1, 0);
+getPoint(1, 1);
+getPoint(3, 3);
 
 function drawGrid() {
-  console.log('  0123456789');
-  for (let y = 0; y < size; y++) {
-    console.log(y + ' ' + grid[y].join(''));
+  let gridSize = grid.reduce((acc, curr) => {
+    return Math.max(acc, curr.length)
+  }, 0);
+  let xrow = '  ';
+  let drow = '--';
+  for (let i = 0; i < gridSize; i++) {
+    xrow += i;
+    drow += '-';
   }
+  console.log(xrow);
+  console.log(drow);
+
+  for (let y = 0; y < gridSize; y++) {
+    if (!grid[y]) {
+      grid[y] = [];
+    }
+    let row = y + ' ';
+    for (let i = 0; i < gridSize; i++) {
+      row += grid[y][i] || '?';
+    }
+    console.log(row);
+  }
+}
+
+drawGrid();
+
+function getNeighbors(pos) {
+
 }
 
 function getDist(start, dest) {
@@ -80,6 +106,5 @@ function getDist(start, dest) {
   }
 }
 
-fillGrid();
-drawGrid();
-console.log('\nDISTANCE:', getDist(start, dest));
+// drawGrid();
+// console.log('\nDISTANCE:', getDist(start, dest));
