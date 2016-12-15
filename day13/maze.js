b3c3c8b0a2b08dd21ae8e650091b04496cb19eef
Marc Majcher <majcher@gmail.com>
2016-12-15 04:07:36 -0600

    starting a*

--- day13/maze.js
+++ day13/maze.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const pq = require('js-priority-queue');
+
 // const data = 1352;
 const data = 10;
 
@@ -37,8 +41,45 @@ function drawGrid() {
 
 function getDist(start, dest) {
   return Math.abs(start.x - dest.x) + Math.abs(start.y - dest.y); // not really
+
+  // from http://www.redblobgames.com/pathfinding/a-star/introduction.html
+  // frontier = PriorityQueue()
+  // frontier.put(start, 0)
+  // came_from = {}
+  // cost_so_far = {}
+  // came_from[start] = None
+  // cost_so_far[start] = 0
+
+  const list = new pq();
+  list.queue(start);
+  const cameFrom = {
+    [JSON.stringify(start)]: undefined
+  };
+  const costSoFar = {
+    [JSON.stringify(start)]: 0
+  };
+
+  // while not frontier.empty():
+  //    current = frontier.get()
+
+  while (list.length > 0) {
+    const current = list.dequeue();
+    if (current.x == dest.x && current.y === dest.y) {
+      break;
+    }
+    //  if current == goal:
+    //     break
+
+    //  for next in graph.neighbors(current):
+    //     new_cost = cost_so_far[current] + graph.cost(current, next)
+    //     if next not in cost_so_far or new_cost < cost_so_far[next]:
+    //        cost_so_far[next] = new_cost
+    //        priority = new_cost + heuristic(goal, next)
+    //        frontier.put(next, priority)
+    //        came_from[next] = current
+  }
 }
 
 fillGrid();
 drawGrid();
-console.log('\nDISTANCE:',getDist(start, dest));
+console.log('\nDISTANCE:', getDist(start, dest));
