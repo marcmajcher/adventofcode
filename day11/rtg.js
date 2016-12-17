ea1ed3ce26533e8835c9540554db5d8690d31f63
Marc Majcher <majcher@gmail.com>
2016-12-16 23:20:44 -0600

    day11 setup

--- day11/rtg.js
+++ day11/rtg.js
@@ -21,11 +21,11 @@ const things = {
 const maxId = 512;
 
 const state = [
-  1,   // Current Floor
-  3,   // Floor 1 : 0000000011
+  1, // Current Floor
+  3, // Floor 1 : 0000000011
   340, // Floor 2 : 0101010100
   680, // Floor 3 : 1010101000
-  0    // Floor 4 : 0000000000
+  0 // Floor 4 : 0000000000
 ];
 
 function itemsOnFloor(floorList, floor) {
@@ -43,26 +43,81 @@ function itemsOnFloor(floorList, floor) {
 function stateToKey(floorData) {
   return floorData.join(':');
 }
+
 function keyToState(key) {
   return key.split(':');
 }
 
-
 function getNeighborStates(floorData) {
+  const floor = floorData[0];
+  const items = itemsOnFloor(floorData, floor);
 
-  return [];
+  const possibleFloors = (floor === 1 ? [2] : floor === 4 ? [3] : [floor - 1, floor + 1]);
+
+  let possibleCargo = [];
+  for (let i = 0; i < items.length; i++) {
+    possibleCargo.push(items[i]);
+    for (let j = i + 1; j < items.length; j++) {
+      possibleCargo.push(items[i] + items[j]);
+    }
+  }
+
+  let states = [];
+  for (let f=0; f<possibleFloors.length; f++) {
+    let newFloor = possibleFloors[f]
+    for (let c=0; c<possibleCargo.length; c++) {
+      let newState = floorData.slice(0);
+      newState[0] = newFloor;
+      newState[floor] -= possibleCargo[c];
+      newState[newFloor] += possibleCargo[c];
+      states.push(newState);
+    }
+  }
+
+  return states;
+}
+
+const generatorBits = [1, 4, 16, 64, 256];
+const microchipBits = [2, 8, 32, 128, 512];
+
+function hasGenerator(data) {
+  return data & 341;
+}
+
+function hasMicrochip(data) {
+  return data & 682;
 }
 
 function stateIsValid(floorData) {
-
+  for (let i = 1; i < floorData.length; i++) {
+    console.log('checking', i);
+    let data = floorData[i];
+    if (hasGenerator(data) && hasMicrochip(data)) {
+      for (let i = 1; i <= maxId; i *= 4) {
+        if (((i * 2) & data) && (!(i & data))) { // has chip without matching generator
+          return false;
+        }
+      }
+    }
+  }
   return true;
+}
+
+function stateIsSuccess(floorData) {
+  return floorData[4] === 1023;
 }
 
 function describeState(floorData) {
   console.log(`You are on floor ${floorData[0]}.`);
   for (let i = 1; i < floorData.length; i++) {
-    console.log(`Items on floor ${i}:`, itemsOnFloor(floorData, i).map(i=>things[i]).join(', '));
+    console.log(`Items on floor ${i}:`, itemsOnFloor(floorData, i).map(i => things[i]).join(', '));
   }
 }
 
 describeState(state);
+console.log();
+getNeighborStates(state).forEach((s) => {
+  describeState(s);
+  console.log('Valid:',stateIsValid(s));
+  console.log();
+});
