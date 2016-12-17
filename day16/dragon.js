8c4ae22ff77f765d1989ec308a3e5ffd58fa9ace
Marc Majcher <majcher@gmail.com>
2016-12-16 21:29:42 -0600

    day16.1

--- day16/dragon.js
+++ day16/dragon.js
@@ -1,3 +1,44 @@
 'use strict';
 
 const input = '10011111011011001';
+const diskSize = 272;
+
+function getDragonData(seed, size) {
+  let data = seed.toString();
+  while (data.length <= size) {
+    let copy = '';
+    for (let i = data.length - 1; i >= 0; i--) {
+      copy += data[i] === '0' ? '1' : '0';
+    }
+    data = `${data}0${copy}`
+  }
+  return data.slice(0, size);
+}
+
+function getCheckSum(str) {
+  const pairSum = {
+    '00': 1,
+    '11': 1,
+    '10': 0,
+    '01': 0
+  }
+
+  let done = false;
+  let cs;
+  while (!done) {
+    cs = '';
+
+    while (str.length > 0) {
+      cs += pairSum[str.slice(0, 2)];
+      str = str.slice(2);
+    }
+    done = cs.length & 1;
+    str = cs;
+  }
+  return cs;
+}
+
+const dragon = getDragonData(input, diskSize);
+// console.log(dragon);
+const checksum = getCheckSum(dragon);
+console.log(checksum);
