c9fb9aaa4953fb1507c6d4ae812859a2cb681340
Marc Majcher <majcher@gmail.com>
2016-12-19 15:27:54 -0600

    day18.1

--- day18/rogue.js
+++ day18/rogue.js
@@ -1,3 +1,31 @@
 'use strict';
 
 const input = '.^^^^^.^^^..^^^^^...^.^..^^^.^^....^.^...^^^...^^^^..^...^...^^.^.^.......^..^^...^.^.^^..^^^^^...^.';
+
+// const rules = ['001', '011', '100', '110']; // => 01011010
+const rules = {
+  '...': '.',
+  '..^': '^',
+  '.^.': '.',
+  '.^^': '^',
+  '^..': '^',
+  '^.^': '.',
+  '^^.': '^',
+  '^^^': '.'
+};
+const rows = 40;
+
+const room = [input];
+
+for (let r=0; r<rows-1; r++) {
+  const row = `.${room[r]}.`;
+  let newRow = '';
+  for (let i=0; i<row.length-2; i++) {
+    newRow += rules[row.slice(i,i+3)];
+    // console.log(row.slice(i,3));
+  }
+  room.push(newRow);
+}
+
+const match = room.join('').match(/\./g) || [];
+console.log(match.length);
