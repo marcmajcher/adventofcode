01e0a88ce11cdd537c58d4bc1883ca79947c7f46
Marc Majcher <majcher@gmail.com>
2016-12-20 01:13:02 -0600

    day20.2

--- day20/firewall.js
+++ day20/firewall.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const input = require('./data');
+const highestIp = 4294967295;
+// const highestIp = 15;
+// const input = [
+//   '0-3',
+//   '6-10'
+// ];
 
 const ips = {};
 input.forEach(i => {
@@ -8,23 +14,23 @@ input.forEach(i => {
   ips[parseInt(start)] = parseInt(end);
 })
 
-let start = 0;
-let keys = Object.keys(ips).sort((a,b) => a-b);
-
-let max = ips[start];
-//
-// keys.forEach(k => {
-//   console.log(k, ips[k]);
-// })
+let keys = Object.keys(ips).sort((a, b) => a - b);
+let max = ips[0];
+let count = 0;
 
 for (var i = 0; i < keys.length; i++) {
   const key = keys[i];
   const value = ips[key];
-  if (key <= max+1) {
+  if (key <= max + 1) {
     max = Math.max(max, value);
   }
   else {
-    console.log(max+1);
-    break;
+    count += key - (max + 1);
+    max = value;
+    if (i === keys.length - 1) {
+      count += highestIp - max;
+    }
   }
 }
+
+console.log('Count', count);
