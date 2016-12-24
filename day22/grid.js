6f0e11263dc4d288fafb48f2e62583185df5e162
Marc Majcher <majcher@gmail.com>
2016-12-23 18:44:59 -0600

    day23

--- day22/grid.js
+++ day22/grid.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const input = require('./data');
+// const input = require('./data');
+const input = [
+  '/dev/grid/node-x0-y0   10T    8T     2T   80%',
+  '/dev/grid/node-x0-y1   11T    6T     5T   54%',
+  '/dev/grid/node-x0-y2   32T   28T     4T   87%',
+  '/dev/grid/node-x1-y0    9T    7T     2T   77%',
+  '/dev/grid/node-x1-y1    8T    0T     8T    0%',
+  '/dev/grid/node-x1-y2   11T    7T     4T   63%',
+  '/dev/grid/node-x2-y0   10T    6T     4T   60%',
+  '/dev/grid/node-x2-y1    9T    8T     1T   88%',
+  '/dev/grid/node-x2-y2    9T    6T     3T   66%'
+]
 
 const nodes = [];
 
@@ -21,8 +32,7 @@ let count = 0;
 nodes.forEach(nodeA => {
   if (nodeA.used !== 0) {
     nodes.forEach(nodeB => {
-      if (nodeA.x === nodeB.x && nodeA.y === nodeB.y) {
-      }
+      if (nodeA.x === nodeB.x && nodeA.y === nodeB.y) {}
       else {
         if (nodeA.used <= nodeB.avail) {
           count++;
@@ -31,5 +41,7 @@ nodes.forEach(nodeA => {
     })
   }
 });
+
+// http://codepen.io/anon/pen/BQEZzK/?editors=1010
 
 console.log(count);
