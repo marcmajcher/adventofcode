18ee0058d48e6fc5d3d989d8e438d5dad5ed1e60
Marc Majcher <majcher@gmail.com>
2016-12-25 02:28:19 -0600

    day25

---
+++ day25/clock.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const input = require('./data.js');
+
+let pc = 0;
+let reg = {
+  a: 0,
+  b: 0,
+  c: 1,
+  d: 0
+}
+
+let output = '';
+
+const cpu = {
+  out: (x) => {
+    output += isNaN(parseInt(x)) ? reg[x] : x;
+    console.log(output);
+    return ++pc;
+  },
+  cpy: (v, r) => {
+    reg[r] = isNaN(parseInt(v)) ? reg[v] : parseInt(v);
+    return ++pc;
+  },
+  inc: (r) => {
+    reg[r]++;
+    return ++pc;
+  },
+  dec: (r) => {
+    reg[r]--;
+    return ++pc;
+  },
+  jnz: (r, d) => {
+    r = isNaN(parseInt(r)) ? reg[r] : parseInt(r);
+    d = isNaN(parseInt(d)) ? reg[d] : parseInt(d);
+
+    return (r === 0 || reg[r] === 0) ?
+      ++pc :
+      pc + parseInt(d);
+  },
+  tgl: (r) => {
+    const ln = reg[r] + pc;
+    const inst = input[ln];
+    if (inst && inst.match(/^(cpy|jnz)/)) {
+      if (inst.match(/^jnz/)) {
+        input[ln] = input[ln].replace(/^jnz/, 'cpy');
+      }
+      else {
+        input[ln] = input[ln].replace(/^.../, 'jnz');
+      }
+    }
+    else if (inst && inst.match(/^(inc|dec|tgl)/)) {
+      if (inst.match(/^inc/)) {
+        input[ln] = input[ln].replace(/^inc/, 'dec');
+      }
+      else {
+        input[ln] = input[ln].replace(/^.../, 'inc');
+      }
+    }
+    return ++pc;
+  }
+};
+
+while (pc < input.length) {
+  // console.log(pc + '  ', input[pc]);
+  let [inst, ...args] = input[pc].split(' ');
+  // console.log(`pc ${pc}: ${input[pc]} => ${JSON.stringify(reg)}`);
+  pc = cpu[inst](...args);
+
+}
+logreg();
+
+function logreg() {
+  console.log(`pc: ${pc} => ${JSON.stringify(reg)}\n`);
+}
