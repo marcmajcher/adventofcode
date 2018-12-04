df6df4232d02ae8c9b3c04e5619a944968e9b148
Marc Majcher <majcher@gmail.com>
2018-12-04 12:15:29 -0600

    day2

--- 2018/day2.js
+++ 2018/day2.js
@@ -4,6 +4,48 @@
 
 const input = require('./day2_input');
 // const input = [
+//   'abcde',
+//   'fghij',
+//   'klmno',
+//   'pqrst',
+//   'fguij',
+//   'axcye',
+//   'wvxyz',
+// ];
+
+function differby(str1, str2) {
+  let diff = 0;
+  for (let i = 0; i < str1.length; i++) {
+    if (str1[i] !== str2[i]) {
+      diff++;
+    }
+  }
+  return diff;
+}
+
+function getPair() {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (differby(input[i], input[j]) === 1) {
+        return [input[i], input[j]];
+      }
+    }
+  }
+  return undefined;
+}
+
+const pair = getPair();
+let out = '';
+for (let i = 0; i < pair[0].length; i++) {
+  if (pair[0][i] === pair[1][i]) {
+    out += pair[0][i];
+  }
+}
+
+console.log(out);
+
+////// pt1
+// const input = [
 //   'abcdef',
 //   'bababc',
 //   'abbcde',
@@ -34,4 +76,4 @@ const input = require('./day2_input');
 //   c3 = has3 ? c3 + 1 : c3;
 // });
 
-console.log(c2 * c3);
+// console.log(c2 * c3);
