d9276000b28a9ee519b6ef4a5f24755e95f9b657
Marc Majcher <marc.majcher@flatironschool.com>
2019-12-19 11:25:08 -0600

    day2

--- 2019/day02.js
+++ 2019/day02.js
@@ -4,4 +4,40 @@
 
 const input = require('./day02_input');
 
-console.log(input)
+function runIntCode(prog, index = 0) {
+  while (index >= 0) {
+    index = runOpCode(prog, index);
+  }
+  return prog[0];
+}
+
+function runOpCode(prog, index) {
+  if (prog[index] === 99) {
+    return -1;
+  }
+  const [op, p1, p2, loc] = prog.slice(index, index + 4);
+  // console.log('index:', index, 'op:', op, 'p1:', p1, 'p2:', p2, 'loc:', loc);
+  if (op === 1) {
+    prog[loc] = prog[p1] + prog[p2];
+  } else if (op === 2) {
+    prog[loc] = prog[p1] * prog[p2];
+  } else {
+    return -1;
+  }
+  return index + 4;
+}
+
+// brute force
+for (let i = 0; i < 100; i++) {
+  for (let j = 0; j < 100; j++) {
+    const memory = input.slice(0);
+    memory[1] = i;
+    memory[2] = j;
+    const result = runIntCode(memory);
+    if (result === 19690720) {
+      console.log(i, j);
+      process.exit();
+    }
+  }
+}
+console.log('bah');
